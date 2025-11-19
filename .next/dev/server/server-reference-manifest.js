8573e3b18becee12bd5360c0ebf916f5bae7f191
syfhinx <syfhinxstallioxbreacher@gmail.com>
2025-11-19 09:58:48 +0300

    movie

---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"KTwEYH0gUrjbbhPdXlLbkOh2RlYKLpGMrgiJ1ZMpvIs=\"\n}"
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default function Home() { 
   return (
-    <div>
-      hello world
+    <div className="align-center flex justify-center p-70"> 
+      <Button>Click me</Button>
+
+      <div className="padding-1 m-15 right margin">
+        <Button> Click me</Button >
+        
+      </div>
+      
     </div>
+    
   );
 }
