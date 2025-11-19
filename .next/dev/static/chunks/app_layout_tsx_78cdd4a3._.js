90342356212c65b1930feb9a21d9ac713902c93b
syfhinx <syfhinxstallioxbreacher@gmail.com>
2025-11-19 11:20:36 +0300

    layout.tsx

--- .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++ .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
@@ -1,7 +1,9 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/[root-of-the-server]__28bc9c2a._.css"
+  "static/chunks/[root-of-the-server]__28bc9c2a._.css",
+  "static/chunks/node_modules_@clerk_nextjs_dist_esm_app-router_452ee46e._.js",
+  "static/chunks/node_modules_a0782223._.js"
 ],
     source: "dynamic"
 });
